ab85de1d8dacfd8c3b0cc63f5ce8f748c9a4ab65
Mariia Glushenkova <mariia.glushenkova@nokia.com>
2025-11-05 23:01:41 +0200

    fix: update TypeScript version and add engine specifications in package.json; refactor Terraform configuration for Azure resources

--- infra/main.tf
+++ infra/main.tf
@@ -1,13 +1,8 @@
 terraform {
-  required_version = ">= 1.6.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.110"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
+      version = "~> 3.70"
     }
   }
 }
@@ -16,59 +11,49 @@ provider "azurerm" {
   features {}
 }
 
-
+# Resource Group (uses provided name OR creates one)
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "random_string" "suffix" {
-     length = 6 
-     special = false 
-     upper = false 
-     }
-
-resource "azurerm_storage_account" "sa" {
-  name                     = "st${var.project_name}${random_string.suffix.result}"
+# Storage account for Function App
+resource "azurerm_storage_account" "storage" {
+  name                     = "${var.project_name}sa"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  allow_nested_items_to_be_public = true
 }
 
+# Serverless Function App Plan
 resource "azurerm_service_plan" "plan" {
-  name                = "asp-${var.project_name}"
+  name                = "${var.project_name}-plan"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
-  sku_name            = "Y1" # Consumption
+  sku_name            = "Y1"
 }
 
+# Function App
 resource "azurerm_linux_function_app" "func" {
-  name                       = "func-${var.project_name}-${random_string.suffix.result}"
+  name                       = "${var.project_name}-func"
   resource_group_name        = azurerm_resource_group.rg.name
   location                   = azurerm_resource_group.rg.location
   service_plan_id            = azurerm_service_plan.plan.id
-  storage_account_name       = azurerm_storage_account.sa.name
-  storage_account_access_key = azurerm_storage_account.sa.primary_access_key
-  functions_extension_version = "~4"
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
 
   site_config {
     application_stack {
-      node_version = "~18"
-    }
-    cors {
-      allowed_origins = [var.cors_allow_origin]
+      node_version = "20"
     }
   }
 
   app_settings = {
-    "AzureWebJobsStorage" = azurerm_storage_account.sa.primary_connection_string
-    "WEBSITE_RUN_FROM_PACKAGE" = "1"
-    "JWT_SECRET"          = var.jwt_secret
-    "BASE_CATALOG_JSON"   = var.base_catalog_json
-    "CLIENT_MAP_JSON"     = var.client_map_json
-    "CORS_ALLOW_ORIGIN"   = var.cors_allow_origin
+    JWT_SECRET         = var.jwt_secret
+    BASE_CATALOG_JSON  = var.base_catalog_json
+    CLIENT_MAP_JSON    = var.client_map_json
+    CORS_ALLOW_ORIGIN  = var.cors_allow_origin
   }
 }
